eeba95419fbabfca4e35a7a952a58167343d1afe
Aarni Koskela <akx@iki.fi>
2017-12-07 15:07:14 +0200

    Initial code

---
+++ goligoi.js
@@ -0,0 +1,96 @@
+const m = window.m;
+let coins = null;
+fetch('https://api.coinmarketcap.com/v1/ticker/')
+    .then((r) => r.json())
+    .then((j) => {
+      coins = j;
+      m.redraw();
+    });
+
+const state = {
+  purchasePrices: {},
+  holdings: {},
+};
+
+Object.assign(state, JSON.parse(localStorage.getItem('goligoi-state') || '{}'));
+
+const setter = (table, symbol) => (event) => {
+  state[table][symbol] =
+      (event.target.value === '' ? null : event.target.valueAsNumber);
+  localStorage.setItem('goligoi-state', JSON.stringify(state));
+};
+
+const numberInput = (props) => (m(
+    'input',
+    Object.assign({type: 'number', step: 0.00001, min: 0}, props),
+    )
+
+);
+
+const coinRow = (coin) => (m('tr', [
+  m('th', coin.symbol),
+  m('th', coin.name),
+  m('td.num', parseFloat(coin.price_usd).toLocaleString('en', {
+    style: 'decimal',
+    minimumFractionDigits: 6,
+    maximumFractionDigits: 6
+  })),
+
+  m('td', numberInput({
+      value: state.purchasePrices[coin.symbol],
+      oninput: setter('purchasePrices', coin.symbol)
+    })),
+  m('td', numberInput({
+      value: state.holdings[coin.symbol],
+      oninput: setter('holdings', coin.symbol)
+    })),
+]));
+
+const coinTable = () =>
+    m('table',
+      m('thead',
+        ['Symbol', 'Name', 'USD', 'Avg Purchase Price', 'Current Holding'].map(
+            (caption) => m('th', caption))),
+      m('tbody', coins.map((coin) => coinRow(coin))));
+
+const resultDiv = () => {
+  let purchaseTotal = 0;
+  let currentTotal = 0;
+  (coins || []).forEach((coin) => {
+    const purchasePrice = state.purchasePrices[coin.symbol] || null;
+    const holding = state.holdings[coin.symbol] || null;
+    if (purchasePrice && holding) {
+      purchaseTotal += purchasePrice * holding;
+      currentTotal += parseFloat(coin.price_usd) * holding;
+    }
+  });
+  if (purchaseTotal != 0) {
+    const percentageString =
+        (currentTotal / purchaseTotal).toLocaleString('en', {
+          style: 'percent',
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 2,
+        });
+    const usdDeltaString = (currentTotal - purchaseTotal).toLocaleString('en', {
+      style: 'currency',
+      currency: 'usd',
+    });
+    const usdString = (currentTotal).toLocaleString('en', {
+      style: 'currency',
+      currency: 'usd',
+    });
+    return m(
+        'div.inner',
+        m('div.percentage', percentageString),
+        m('div.fiat', usdString),
+        m('div.fiat', 'Δ ' + usdDeltaString),
+    );
+  }
+};
+
+const view = () => {
+  if (coins === null) return m('div', 'Loading from Coinmarketcap...');
+  return m('main', m('div#table', coinTable()), m('div#result', resultDiv()));
+};
+
+m.mount(document.body, {view});
