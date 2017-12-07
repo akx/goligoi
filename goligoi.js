f0554774273de15339c65077d6e2d75f7d04966d
Aarni Koskela <akx@iki.fi>
2017-12-07 15:12:04 +0200

    Allow zero purchase price

--- goligoi.js
+++ goligoi.js
@@ -57,35 +57,35 @@ const resultDiv = () => {
   let purchaseTotal = 0;
   let currentTotal = 0;
   (coins || []).forEach((coin) => {
-    const purchasePrice = state.purchasePrices[coin.symbol] || null;
+    const purchasePrice = state.purchasePrices[coin.symbol] || 0;
     const holding = state.holdings[coin.symbol] || null;
-    if (purchasePrice && holding) {
+    if (holding) {
       purchaseTotal += purchasePrice * holding;
       currentTotal += parseFloat(coin.price_usd) * holding;
     }
   });
-  if (purchaseTotal != 0) {
-    const percentageString =
-        (currentTotal / purchaseTotal).toLocaleString('en', {
-          style: 'percent',
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 2,
-        });
-    const usdDeltaString = (currentTotal - purchaseTotal).toLocaleString('en', {
-      style: 'currency',
-      currency: 'usd',
-    });
-    const usdString = (currentTotal).toLocaleString('en', {
-      style: 'currency',
-      currency: 'usd',
-    });
-    return m(
-        'div.inner',
-        m('div.percentage', percentageString),
-        m('div.fiat', usdString),
-        m('div.fiat', 'Δ ' + usdDeltaString),
-    );
-  }
+  if (currentTotal === 0) return;
+  const percentageString =
+      (purchaseTotal > 0 ? (currentTotal / purchaseTotal).toLocaleString('en', {
+        style: 'percent',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+      }) :
+                           '∞');
+  const usdDeltaString = (currentTotal - purchaseTotal).toLocaleString('en', {
+    style: 'currency',
+    currency: 'usd',
+  });
+  const usdString = (currentTotal).toLocaleString('en', {
+    style: 'currency',
+    currency: 'usd',
+  });
+  return m(
+      'div.inner',
+      m('div.percentage', percentageString),
+      m('div.fiat', usdString),
+      m('div.fiat', 'Δ ' + usdDeltaString),
+  );
 };
 
 const view = () => {
