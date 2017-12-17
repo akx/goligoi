8206257001e1204f9a329c53c9510820e11a6563
Aarni Koskela <akx@iki.fi>
2017-12-17 14:12:43 +0200

    Add key to the table

--- goligoi.js
+++ goligoi.js
@@ -3,89 +3,84 @@ let coins = null;
 
 function getCoins() {
   fetch('https://api.coinmarketcap.com/v1/ticker/')
-      .then((r) => r.json())
-      .then((j) => {
-        coins = j;
-        m.redraw();
-      });
+    .then(r => r.json())
+    .then(j => {
+      coins = j;
+      m.redraw();
+    });
 }
 
 const state = {
   purchasePrices: {},
-  holdings: {},
+  holdings: {}
 };
 
 Object.assign(state, JSON.parse(localStorage.getItem('goligoi-state') || '{}'));
 
-const setter = (table, symbol) => (event) => {
-  state[table][symbol] =
-      (event.target.value === '' ? null : event.target.valueAsNumber);
+const setter = (table, symbol) => event => {
+  state[table][symbol] = event.target.value === '' ? null : event.target.valueAsNumber;
   localStorage.setItem('goligoi-state', JSON.stringify(state));
 };
 
-const numberInput = (props) => (m(
-    'input',
-    Object.assign({type: 'number', step: 0.00001, min: 0}, props),
-    ));
+const numberInput = props => m('input', Object.assign({ type: 'number', step: 0.00001, min: 0 }, props));
 
-const formatUSD = (usd) => usd.toLocaleString('en', {
-  style: 'currency',
-  currency: 'usd',
-});
+const formatUSD = usd =>
+  usd.toLocaleString('en', {
+    style: 'currency',
+    currency: 'usd'
+  });
 
 const formatPercentage = (p, d = 0) =>
-    ((p !== 0 && !isNaN(p)) ? (p + d).toLocaleString('en', {
-      style: 'percent',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
-    }) :
-                              '∞');
+  p !== 0 && !isNaN(p)
+    ? (p + d).toLocaleString('en', {
+        style: 'percent',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+      })
+    : '∞';
 
-const coinRow = (coin) => {
+const coinRow = coin => {
   const currentPrice = parseFloat(coin.price_usd);
   const currentHolding = state.holdings[coin.symbol];
   const currentHoldingValue = currentPrice * currentHolding;
-  const purchaseValue =
-      (state.purchasePrices[coin.symbol] || 0) * currentHolding;
-  return m('tr', [
+  const purchaseValue = (state.purchasePrices[coin.symbol] || 0) * currentHolding;
+  return m('tr', { key: coin.symbol }, [
     m('th', coin.symbol),
     m('th', coin.name),
-    m('td.num', currentPrice.toLocaleString('en', {
-      style: 'decimal',
-      minimumFractionDigits: 6,
-      maximumFractionDigits: 6
-    })),
+    m(
+      'td.num',
+      currentPrice.toLocaleString('en', {
+        style: 'decimal',
+        minimumFractionDigits: 6,
+        maximumFractionDigits: 6
+      })
+    ),
 
-    m('td', numberInput({
+    m(
+      'td',
+      numberInput({
         value: state.purchasePrices[coin.symbol],
         oninput: setter('purchasePrices', coin.symbol)
-      })),
-    m('td',
-      numberInput(
-          {value: currentHolding, oninput: setter('holdings', coin.symbol)})),
-    m('td.num',
-      (currentHolding ?
-           formatPercentage(currentHoldingValue / purchaseValue, -1) :
-           null)),
-    m('td.num', (currentHolding ? formatUSD(currentHoldingValue) : null)),
-    m('td.num',
-      (currentHolding ? formatUSD(currentHoldingValue - purchaseValue) : null)),
+      })
+    ),
+    m('td', numberInput({ value: currentHolding, oninput: setter('holdings', coin.symbol) })),
+    m('td.num', currentHolding ? formatPercentage(currentHoldingValue / purchaseValue, -1) : null),
+    m('td.num', currentHolding ? formatUSD(currentHoldingValue) : null),
+    m('td.num', currentHolding ? formatUSD(currentHoldingValue - purchaseValue) : null)
   ]);
 };
 
 const coinTable = () =>
-    m('table',
-      m('thead',
-        [
-          'Symbol', 'Name', 'USD', 'Avg Purch $', 'Current Holding', '%', '$',
-          'Δ$'
-        ].map((caption) => m('th', caption))),
-      m('tbody', coins.map((coin) => coinRow(coin))));
+  m(
+    'table',
+    m('thead', ['Symbol', 'Name', 'USD', 'Avg Purch $', 'Current Holding', '%', '$', 'Δ$'].map(caption => m('th', caption))),
+    m('tbody', coins.map(coin => coinRow(coin)))
+  );
 
 const resultDiv = () => {
   let purchaseTotal = 0;
   let currentTotal = 0;
-  (coins || []).forEach((coin) => {
+  (coins || []).forEach(coin => {
     const purchasePrice = state.purchasePrices[coin.symbol] || 0;
     const holding = state.holdings[coin.symbol] || null;
     if (holding) {
@@ -94,15 +89,10 @@ const resultDiv = () => {
     }
   });
   if (currentTotal === 0) return;
-  const percentageString = formatPercentage((currentTotal / purchaseTotal), -1);
+  const percentageString = formatPercentage(currentTotal / purchaseTotal, -1);
   const usdDeltaString = formatUSD(currentTotal - purchaseTotal);
   const usdString = formatUSD(currentTotal);
-  return m(
-      'div.inner',
-      m('div.percentage', percentageString),
-      m('div.fiat', usdString),
-      m('div.fiat', 'Δ ' + usdDeltaString),
-  );
+  return m('div.inner', m('div.percentage', percentageString), m('div.fiat', usdString), m('div.fiat', 'Δ ' + usdDeltaString));
 };
 
 const view = () => {
@@ -110,9 +100,9 @@ const view = () => {
   return m('main', [
     m('div#table', coinTable()),
     m('div#result', resultDiv()),
-    m('button#reload', {onclick: () => getCoins()}, 'Refresh'),
+    m('button#reload', { onclick: () => getCoins() }, 'Refresh')
   ]);
 };
 
-m.mount(document.body, {view});
+m.mount(document.body, { view });
 getCoins();
